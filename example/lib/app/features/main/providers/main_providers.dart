33d394a759f609ac669b0e034590410de74875ec
GJJ2019 <gauravjajoo98@gmail.com>
2022-07-11 10:20:29 +0530

    - changing icon - adding key to store data

--- example/lib/app/features/main/providers/main_providers.dart
+++ example/lib/app/features/main/providers/main_providers.dart
@@ -1,15 +1,20 @@
 import 'package:easy_upi_payment/easy_upi_payment.dart';
 import 'package:hooks_riverpod/hooks_riverpod.dart';
 
+import '../../../core/local_storage/app_storage.dart';
+
 class MainStateNotifier extends StateNotifier<MainState> {
-  MainStateNotifier() : super(MainState.initial);
+  MainStateNotifier(this.ref) : super(MainState.initial);
 
   TransactionDetailModel? transactionDetailModel;
+
+  final Ref ref;
 
   /// for starting payment
   Future<void> startPayment(EasyUpiPaymentModel model) async {
     state = MainState.loading;
     try {
+      savePaymentData(model);
       //
       final res = await EasyUpiPaymentPlatform.instance.startPayment(model);
       if (res != null) {
@@ -22,11 +27,23 @@ class MainStateNotifier extends StateNotifier<MainState> {
       state = MainState.error;
     }
   }
+
+  /// for saving payment data in hive
+  void savePaymentData(EasyUpiPaymentModel model) {
+    ref.read(appStorageProvider)
+      ..putName(model.payeeName)
+      ..putUpiId(model.payeeVpa)
+      ..putAmount(model.amount.toString());
+
+    if (model.description != null) {
+      ref.read(appStorageProvider).putDescription(model.description!);
+    }
+  }
 }
 
 final mainStateProvider = StateNotifierProvider.autoDispose<MainStateNotifier, MainState>(
   (ref) {
-    return MainStateNotifier();
+    return MainStateNotifier(ref);
   },
 );
 
